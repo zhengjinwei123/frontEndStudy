20b827644534050c6d241664f109828dbaa9106c
zhengjinwei123 <2538698032@qq.com>
2017-08-11 09:18:59 +0800

    update

--- ali_blog/public/js/common.js
+++ ali_blog/public/js/common.js
@@ -22,7 +22,7 @@
 
     // 浏览器提示
     $(function () {
-        var isChrome = navigator.userAgent.toLowerCase().match(/chrome/) != null;
+        var isChrome = navigator.userAgent.toLowerCase().match(/chrome/) !== null;
         if (!isChrome) {
             var element = $('#browser_notice_dialog');
             element.modal({backdrop: 'static'});
@@ -121,9 +121,9 @@
     String.prototype.format = function (args) {
         var result = this;
         if (arguments.length > 0) {
-            if (arguments.length == 1 && typeof (args) == "object") {
+            if (arguments.length === 1 && typeof (args) === "object") {
                 for (var key in args) {
-                    if (args[key] != undefined) {
+                    if (args[key] !== undefined) {
                         var reg = new RegExp("({" + key + "})", "g");
                         result = result.replace(reg, args[key]);
                     }
@@ -131,7 +131,7 @@
             }
             else {
                 for (var i = 0; i < arguments.length; i++) {
-                    if (arguments[i] != undefined) {
+                    if (arguments[i] !== undefined) {
                         //var reg = new RegExp("({[" + i + "]})", "g");//这个在索引大于9时会有问题，谢谢何以笙箫的指出
                         var reg = new RegExp("({)" + i + "(})", "g");
                         result = result.replace(reg, arguments[i]);
